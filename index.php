f6b32a2230afe9c6be6cd034c748032e4e485c14
StayZe <mandra.antoine@gmail.com>
2025-01-24 16:45:41 +0100

    .

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require_once 'routes/trainRoutes.php';
 require_once 'routes/repairRoutes.php';
 
